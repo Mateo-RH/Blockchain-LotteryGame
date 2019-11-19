221e5c0498e3712da323a4006ce1261867c535d6
MateoRH <marami26@eafit.edu.co>
2019-11-18 19:17:54 -0500

    Dockerfile and Heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { createServer } = require('http');
 const next = require('next');
-
+process.env.PORT = process.env.PORT || 3000;
 // TODO: Ambiente productivo
 const app = next({
   dev: process.env.NODE_ENV !== 'production'
@@ -10,8 +10,8 @@ const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000, err => {
+  createServer(handler).listen(process.env.PORT, err => {
     if (err) throw err;
-    console.log('Ready on localhost:3000');
+    console.log(`Ready on localhost: ${process.env.PORT}`);
   });
 });
